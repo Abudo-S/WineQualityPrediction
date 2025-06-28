a505f209dc74e3d67b53fb316eb93342f1aa1647
Abudo-S <abudo.mohamed123456789@gmail.com>
2025-06-28 20:26:38 +0200

    Implement Kernel class with three kernel funtions.

---
+++ Kernel.py
@@ -0,0 +1,55 @@
+import numpy as np  
+
+'''
+Contains non-linear kernals to be used on the target model
+'''
+class Kernel:
+    '''
+    The polynomial kernel is generally used if you have some domain knowledge, deducing that the features have polynomial relationships.
+    (ex. x1 = x2^2)
+    '''
+    def polynomial_kernel(self, x1, x2):
+        degree = self.kernel_params.get('degree', 3)
+        gamma = self.kernel_params.get('gamma', 1.0)
+        r = self.kernel_params.get('r', 0.0)
+
+        return (gamma * np.dot(x1, x2) + r) ** degree
+
+    '''
+    The gaussian kernel is a general-purpose kernel which can considered as the first non-linear kernel to try 
+    when we suspect that the data are not linearly separable.
+    It captures non-linear relationships which are not necessarily polynomial.
+    '''
+    def gaussian_kernel(self, x1, x2):
+        gamma = self.kernel_params.get('gamma', 1.0)
+        
+        return np.exp(-gamma * np.sum((x1 - x2)**2))
+    
+    '''
+    The sigmoid kernel takes two data points and calculates a similarity score based on the tanh function.
+    The score reflects how similar the two data points are in a higher-dimensional space.
+    It's generally used in neural networks and image processing.
+    '''
+    def sigmoid_kernal(self, x1, x2):
+        gamma = self.kernel_params.get('gamma', 1.0)
+        r = self.kernel_params.get('r', 0.0)
+
+        return np.tanh(gamma * np.dot(x1, x2) + r)
+
+    def __init__(self, X:np.ndarray, kernal_func=gaussian_kernel, kernel_params:dict=None):
+        self.kernal_func = kernal_func
+        self.kernel_params = kernel_params
+        self.K_matrix = self._compute_kernel_matrix(X)
+ 
+    '''
+    Computes the kernel matrix "K" where K_ij = K(x_i, x_j).
+    '''
+    def _compute_kernel_matrix(self, X):
+        n_samples = X.shape[0]
+        K = np.zeros((n_samples, n_samples))
+
+        for i in range(n_samples):
+            for j in range(n_samples):
+                K[i, j] = self.kernal_func(X[i], X[j])
+
+        return K
